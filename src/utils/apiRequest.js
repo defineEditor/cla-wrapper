d39d5589ae9a5213a2d8a2cba42ee93094a9cff1
Dmitry <dmitry.m.kolosov@gmail.com>
2020-08-30 18:03:38 +0300

    Adding OAuth2

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -1,19 +1,25 @@
 const { promisify } = require('util');
 const request = promisify(require('request'));
 
-const apiRequest = async ({ username, password, url, headers = {}, cache }) => {
+const apiRequest = async ({ username, password, apiKey, url, headers = {}, cache }) => {
     const req = {
         url,
         headers: {
             Accept: 'application/json',
             ...headers,
         },
-        auth: {
+    };
+    if (apiKey !== undefined) {
+        // OAuth2 authentication
+        req.headers['Ocp-Apim-Subscription-Key'] = apiKey;
+    } else {
+        // Basic authentication
+        req.auth = {
             user: username,
             pass: password,
             sendImmediately: false
-        }
-    };
+        };
+    }
     if (headers.Accept && headers.Accept === 'application/vnd.ms-excel') {
         // Set encoding to null, as response is binary
         req.encoding = null;
